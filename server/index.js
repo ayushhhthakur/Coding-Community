b8184becf754c11939b0171d3ed5d9c64cf7800f
Ayush Thakur <ayushthakur1412@gmail.com>
2024-02-10 14:31:35 +0530

    Fix imports and typos, update CORS configuration

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
@@ -18,21 +18,23 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-// mongoose.set('strictQuery', false);
-
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("comman"));
+app.use(morgan("common")); // corrected typo: "comman" to "common"
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+// CORS Configuration
+app.use(cors({
+  origin: 'https://server-coding-community.vercel.app'
+}));
+
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE
@@ -47,8 +49,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-//ROUTES WITH FILES
-app.post("/auth/register/", upload.single("picture"), register);
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register); // Removed extra forward slash
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
@@ -56,15 +58,14 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-// MONGOOSE SETUP 
-
+// MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    //ADD DATA ONE TIME
+    // ADD DATA ONE TIME
     User.insertMany(users);
     Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
