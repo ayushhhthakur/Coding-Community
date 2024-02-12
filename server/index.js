e1ee0ec3a7539c12c0bcc1fd7fd3ab1d0a7ed655
Ayush Thakur <ayushthakur1412@gmail.com>
2024-02-12 22:28:29 +0530

    Update CORS origin URL

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors({
-    origin: 'https://codingcommunity.vercel.app', // Corrected origin URL
+    origin: 'https://codingcommunity.vercel.app',
     credentials: true,
 }));
 
