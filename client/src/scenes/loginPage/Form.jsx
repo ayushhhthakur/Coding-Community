5cf1eddfb848e21b843d9b01672c29b7fb3df620
Ayush Thakur <ayushthakur1412@gmail.com>
2024-02-17 13:28:46 +0530

    Fixeds

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -63,7 +63,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://server-coding-community/auth/register",
+      "/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch(
-      "https://server-coding-community/auth/login",
+      "/auth/login",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
